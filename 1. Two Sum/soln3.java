7453b446c1d4d0d7cd5af3cf81aded8edf86b1f2
anandha2017 <github2017@ponnampalam.ca>
2025-04-19 18:50:27 +0100

    working java

--- 1. Two Sum/soln3.java
+++ 1. Two Sum/soln3.java
@@ -1,25 +1,29 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-public class soln2 {
+public class soln3 {
 
     class Solution {
-        public int[] twoSum(int[] nums, int target) {           
-            
-            for (int i = 0; i < nums.length - 1; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[i] + nums[j] == target) {
-                        return new int[]{i,j};
-                    }
-                }
-            }
+        public int[] twoSum(int[] nums, int target) {      
 
+            int comp; 
+            Map<Integer, Integer> seen = new HashMap<>();
+
+            for (int i = 0; i < nums.length; i++) {
+                comp = target - nums[i];
+                if (seen.get(comp) != null) {
+                    return new int[]{seen.get(comp),i};
+                }
+                seen.put(nums[i],i);
+            }
             throw new IllegalArgumentException("No two sum solution");
         }
     }
 
     public static void main(String[] args) {
         // Create an instance of the outer class
-        soln2 outer = new soln2();
+        soln3 outer = new soln3();
 
         // Create an instance of the inner Solution class
         Solution solution = outer.new Solution();
