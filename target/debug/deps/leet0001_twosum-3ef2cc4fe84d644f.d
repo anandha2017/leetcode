a4912f3e5e4bedb3d9f17270250f7f309e0dff7e
anandha2017 <github2017@ponnampalam.ca>
2025-04-19 20:04:24 +0100

    Cleaned up folders, source code now in its own lang folder

--- target/debug/deps/leet0001_twosum-3ef2cc4fe84d644f.d
+++
@@ -1,5 +0,0 @@
-/Users/anandha/GitHub/leetcode/target/debug/deps/leet0001_twosum-3ef2cc4fe84d644f: Leet0001_TwoSum/src/main.rs
-
-/Users/anandha/GitHub/leetcode/target/debug/deps/leet0001_twosum-3ef2cc4fe84d644f.d: Leet0001_TwoSum/src/main.rs
-
-Leet0001_TwoSum/src/main.rs:
--- target/debug/leet0001_twosum.d
+++
@@ -1,1 +0,0 @@
-/Users/anandha/GitHub/leetcode/target/debug/leet0001_twosum: /Users/anandha/GitHub/leetcode/Leet0001_TwoSum/src/main.rs
