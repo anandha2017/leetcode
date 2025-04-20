50187943fabf4713e9ff305bc6c5233f3e3e2b44
anandha2017 <github2017@ponnampalam.ca>
2025-04-20 16:29:49 +0100

    Typescript hello world

---
+++ Leet0001_TwoSum/ts/hello.js
@@ -0,0 +1,7 @@
+/*
+ * Basic Hello World in TypeScript
+ */
+// Define a typed message
+var message = 'Hello, World!';
+// Log the message to the console
+console.log(message);
---
+++ Leet0001_TwoSum/ts/hello.ts
@@ -0,0 +1,9 @@
+/*
+ * Basic Hello World in TypeScript
+ */
+
+// Define a typed message
+const message: string = 'Hello, World!';
+
+// Log the message to the console
+console.log(message);
