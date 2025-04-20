4af6fe225cf3375561ad0605a778f08e4597ee4d
anandha2017 <github2017@ponnampalam.ca>
2025-04-20 18:16:31 +0100

    Typescript done

--- Leet0009_PalindromeNumber/soln.ts
+++ Leet0009_PalindromeNumber/soln.ts
@@ -1,4 +1,20 @@
-class Solution:
+
+function isPalindrome(x: number): boolean {
+    if (x < 0) {
+        return false
+    }
+ 
+    let reverse = 0
+    let temp = x
+    while (temp != 0) {
+        reverse = ((reverse * 10) + (temp % 10));
+        temp = Math.trunc(temp / 10);
+    }
+    
+    return (reverse == x)
+};
+
+/* class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
             return False
@@ -16,3 +32,8 @@ solution = Solution()
 print(solution.isPalindrome(121)   == True)    # example 1
 print(solution.isPalindrome(-121)  == False)   # example 2
 print(solution.isPalindrome(10)    == False)   # example 3
+ */
+
+console.log(isPalindrome(121)==true)
+console.log(isPalindrome(-121)==false)
+console.log(isPalindrome(10)==false)
