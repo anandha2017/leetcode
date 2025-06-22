ef6564f389c430d4edf9b5a454d90a9d5d433da6
anandha2017 <github2017@ponnampalam.ca>
2025-06-22 23:00:27 +0100

    Before a Cline fix

--- Leet0219_ContainsDuplicate2/Soln.java
+++ Leet0219_ContainsDuplicate2/Soln.java
@@ -4,6 +4,22 @@ public class Soln {
     class Solution {
         public boolean containsNearbyDuplicate(int[] nums, int k) {
 
+            if (nums.length == 1) {
+                return false;
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = 0; j < nums.length; j++) {
+                    if (i != j) {
+                        if (nums[i] == nums[j]) {
+                            if (Math.abs(i - j) <= k) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
             return false;
         }
     }
